c4da27aacc9534d2f962570f17aeeea7ce62469f
Andreloren <andre.lorente83@gmail.com>
2023-05-23 17:49:17 -0300

    cria logo

---
+++ web/src/shared/components/logo/Logo.tsx
@@ -0,0 +1,5 @@
+import { LogoStyle } from "./";
+
+export const Logo: React.FC = () => {
+  return <LogoStyle src="./Chat3d.jpg" alt="Balões de Bate papo" />;
+};
---
+++ web/src/shared/components/logo/LogoStyled.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const LogoStyle = styled.img`
+  width: 120px;
+  height: 110px;
+  margin-top: 10px;
+`;
